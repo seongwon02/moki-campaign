359463ea1461d8f7cb4fbf55f39120d68571fafe
Dohyeong Park <bagdohyeong0@gmail.com>
2025-10-10 15:29:33 +0900

    chore(fe): javascript 기반에서 typescript 기반으로 변경

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,29 +1,42 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{js,jsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      reactHooks.configs['recommended-latest'],
+      // tseslint.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
+
+      reactX.configs["recommended-typescript"],
+      reactDom.configs.recommended,
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
+        // Vite 프로젝트의 앱용 및 노드용 tsconfig 파일을 모두 지정
+        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+        tsconfigRootDir: import.meta.dirname,
       },
     },
+
+    // 5. 필요에 따라 개별 규칙을 완화/강화
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      // 예를 들어, strictTypeChecked가 과도하게 엄격하다고 느끼는 규칙을 완화
+      // '@typescript-eslint/strict-boolean-expressions': 'warn',
     },
   },
-])
+]);

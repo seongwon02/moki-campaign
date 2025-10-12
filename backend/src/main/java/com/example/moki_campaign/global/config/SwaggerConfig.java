59f6bca5bd0559b0ee1a5ae9cf72dc5a114022ce
seongwon02 <seongwon2303@gmail.com>
2025-10-12 17:11:44 +0900

    feat(be): swagger설정 클래스 구현

---
+++ backend/src/main/java/com/example/moki_campaign/global/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.example.moki_campaign.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .servers(List.of(
+                        new Server().url("http://localhost:8080").description("로컬 서버")
+                        // new Server().url("https://api.moki-campaign.com").description("개발 서버")
+                ))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("JWT 토큰을 입력하세요")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
+    }
+}

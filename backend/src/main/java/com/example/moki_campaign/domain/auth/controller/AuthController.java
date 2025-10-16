3f50cd7cb5ab9f342844b8f85249be896e63b535
Kim Seongwon <91422659+seongwon02@users.noreply.github.com>
2025-10-17 02:56:13 +0900

    fix(be): HttpStatus import문 추가

--- backend/src/main/java/com/example/moki_campaign/domain/auth/controller/AuthController.java
+++ backend/src/main/java/com/example/moki_campaign/domain/auth/controller/AuthController.java
@@ -7,6 +7,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
